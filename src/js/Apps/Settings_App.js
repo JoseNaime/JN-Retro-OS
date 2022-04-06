54a4ba1d4a5cd591ca497d604a4b80e85e03e9e9
Jose Naime <josep.naimeg@gmail.com>
2022-04-06 01:54:28 -0500

    Change background color functionality

--- src/js/Apps/Settings_App.js
+++ src/js/Apps/Settings_App.js
@@ -1,27 +1,11 @@
+import CONSTANTS from "../CONSTANTS.js";
+
 class Settings_App {
     constructor() {
         this.backgroundIndex = 0;
-        this.backgrounds = [
-            {
-                name: 'Dots 4px',
-                url: './src/img/backgrounds/dots_4px.png',
-                preview: './src/img/backgrounds/previews/dots_4px_prev.png'
-            },
-            {
-                name: 'Dots 6px',
-                url: './src/img/backgrounds/dots_6px.png',
-                preview: './src/img/backgrounds/previews/dots_6px_prev.png'
+        this.setBackground()
 
-            },
-            {
-                name: 'Dots 8px',
-                url: './src/img/backgrounds/dots_8px.png',
-                preview: './src/img/backgrounds/previews/dots_8px_prev.png'
-            }
-        ]
-
-        this.setBackground(this.backgroundIndex)
-
+        // --- PATTERN ---
         // set previous background button
         $('#btn-next-background').click(() => {
             this.nextBackground();
@@ -31,31 +15,97 @@ class Settings_App {
             this.prevBackground();
         })
 
+        this.dynamicColorElements = $('*:not(.no-dynamic, html)')
+
+        // --- COLOR 1
+        // set next color_1 button
+        this.color1Index = 0;
+        $('#btn-next-color_1').click(() => {
+            this.nextColor(1);
+        })
+        // set prev color_1 button
+        $('#btn-prev-color_1').click(() => {
+            this.prevColor(1);
+        })
+
+        this.setColor(1);
+
+
     }
 
-    setBackground(index) {
+    setBackground() {
         const backgroundElement = $('#background');
-        const currentBackground = this.backgrounds[index];
+        const currentBackground = CONSTANTS.BACKGROUNDS[this.backgroundIndex];
         backgroundElement.css('background-image', `url(${currentBackground.url})`);
         $('#background-preview').css('background-image', `url(${currentBackground.preview})`);
         $('#background-name').html(currentBackground.name);
     }
 
+    setColor(section){
+        const colorPrev =  $('#color_' + section + '-preview');
+        let currentColor;
+
+        if (section === 1){
+            currentColor = CONSTANTS.LIGHT_COLORS[this.color1Index];
+            // Only set Background
+            $('#color_1-name').html(currentColor.name);
+            this.dynamicColorElements.css('background-color', `${currentColor.hex}`);
+        } else {
+            currentColor = CONSTANTS.DARK_COLORS[this.color2Index];
+            // Set border, color and fill
+            this.dynamicColorElements.css('border-color', `${currentColor.hex}`);
+            this.dynamicColorElements.css('color', `${currentColor.hex}`);
+        }
+        console.log(currentColor)
+        colorPrev.css('background-color', currentColor.hex);
+
+    }
+
+    nextColor(section){
+        if (section === 1){
+            this.color1Index++;
+            if (this.color1Index >= CONSTANTS.LIGHT_COLORS.length) {
+                this.color1Index = 0;
+            }
+        }else{
+            this.color2Index++;
+
+            if (this.color2Index >= CONSTANTS.DARK_COLORS.length) {
+                this.color2Index = 0;
+            }
+        }
+        this.setColor(section)
+    }
+
+    prevColor(section){
+        if (section === 1){
+            this.color1Index--;
+            if (this.color1Index < 0) {
+                this.color1Index = CONSTANTS.LIGHT_COLORS.length - 1;
+            }
+        }else{
+            this.color2Index--;
+            if (this.color2Index < 0) {
+                this.color2Index = CONSTANTS.DARK_COLORS.length - 1;
+            }
+        }
+        this.setColor(section)
+    }
+
     nextBackground() {
         this.backgroundIndex++;
-        console.log(this.backgrounds.length);
-        if (this.backgroundIndex >= this.backgrounds.length) {
+        if (this.backgroundIndex >= CONSTANTS.BACKGROUNDS.length) {
             this.backgroundIndex = 0;
         }
-        this.setBackground(this.backgroundIndex);
+        this.setBackground();
     }
 
     prevBackground() {
         this.backgroundIndex--;
         if (this.backgroundIndex < 0) {
-            this.backgroundIndex = this.backgrounds.length - 1;
+            this.backgroundIndex = CONSTANTS.BACKGROUNDS.length - 1;
         }
-        this.setBackground(this.backgroundIndex);
+        this.setBackground();
     }
 
 }
---
+++ src/js/CONSTANTS.js
@@ -0,0 +1,95 @@
+class CONSTANTS {
+    static BACKGROUNDS =
+        [
+            {
+                name: 'Dots 4px',
+                url: './src/img/backgrounds/dots_4px.png',
+                preview: './src/img/backgrounds/previews/dots_4px_prev.png',
+            },
+            {
+                name: 'Dots 6px',
+                url: './src/img/backgrounds/dots_6px.png',
+                preview: './src/img/backgrounds/previews/dots_6px_prev.png'
+
+            },
+            {
+                name: 'Dots 8px',
+                url: './src/img/backgrounds/dots_8px.png',
+                preview: './src/img/backgrounds/previews/dots_8px_prev.png'
+            }
+        ]
+    static LIGHT_COLORS = [
+        {
+            name: 'White',
+            hex: '#FFFFFF'
+        },
+        {
+            name: 'Gray',
+            hex: '#C2C2C2'
+        },
+        {
+            name: 'Red',
+            hex: '#FCE3E3'
+        },
+        {
+            name: 'Orange',
+            hex: '#F8C9C9'
+        },
+        {
+            name: 'Yellow',
+            hex: '#F9E3C9'
+        },
+        {
+            name: 'Green',
+            hex: '#C9F9E3'
+        },
+        {
+            name: 'Blue',
+            hex: '#c9dcf9'
+        },
+        {
+            name: 'Purple',
+            hex: '#d7c9f9'
+        }
+    ]
+
+    static DARK_COLORS = [
+        {
+            name: 'Black',
+            hex: '#000000'
+        },
+        {
+            name: 'Gray',
+            hex: '#343434'
+        },
+        {
+            name: 'Red',
+            hex: '#461f1f'
+        },
+        {
+            name: 'Orange',
+            hex: '#502d1a'
+        },
+        {
+            name: 'Yellow',
+            hex: '#5d522e'
+        },
+        {
+            name: 'Green',
+            hex: '#30410f'
+        },
+        {
+            name: 'Blue',
+            hex: '#2a3554'
+        },
+        {
+            name: 'Purple',
+            hex: '#4f2b5b'
+        }
+
+    ]
+
+
+}
+
+export default CONSTANTS
